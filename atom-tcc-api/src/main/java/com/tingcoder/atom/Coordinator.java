6f1caeab7e24c0b67f886e4d76c9929b02c45af6
yangfeng <yangfeng3@beingmate.com>
2018-11-12 16:03:07 +0800

    add api docs

--- atom-tcc-api/src/main/java/com/tingcoder/atom/Coordinator.java
+++ atom-tcc-api/src/main/java/com/tingcoder/atom/Coordinator.java
@@ -7,6 +7,13 @@ import java.util.List;
 
 public interface Coordinator {
 
+    /**
+     * 开启一个分布式事务
+     * @param sequenceId
+     * @param procedures
+     * @return 全局唯一的分布式事务ID
+     * @throws CoordinatorException
+     */
     long begin(int sequenceId, List<Procedure> procedures) throws CoordinatorException;
 
     short confirm(int sequenceId, long uuid, List<Procedure> procedures) throws CoordinatorException;
